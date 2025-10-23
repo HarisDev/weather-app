ada7ac23cfaf8818eb9e7f6760f5191feb84a100
HarisDev <haris.muharemovic@outlook.com>
2025-10-23 16:22:42 +0200

    refactor: move geo ip api url into the environment variable

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
-export const GEO_IP_API = "http://ip-api.com/json";
+export const GEO_IP_API = import.meta.env.VITE_GEO_IP_API || "http://ip-api.com/json";
 
 export const GOOGLE_WEATHER_API_KEY = import.meta.env.VITE_GOOGLE_WEATHER_API_KEY || "";
 export const GOOGLE_WEATHER_API_BASE_URL = import.meta.env.VITE_GOOGLE_WEATHER_API_BASE_URL || "https://weather.googleapis.com/v1";
